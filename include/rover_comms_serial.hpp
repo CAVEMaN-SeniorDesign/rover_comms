6242faa4ac986acfec91f3f81809754e251a5748
delta-12 <btesq235@gmail.com>
2025-03-31 13:53:13 -0400

    SD-334 default args fix again?

--- include/rover_comms_serial.hpp
+++ include/rover_comms_serial.hpp
@@ -12,13 +12,13 @@ namespace rover_comms
 class CaveTalkSerialPort : public serial::Serial
 {
     public:
-        CaveTalkSerialPort(const std::string &port           = "",
-                           uint32_t baudrate                 = 1e6,
-                           serial::Timeout timeout           = serial::Timeout::simpleTimeout(0),
-                           serial::bytesize_t bytesize       = serial::eightbits,
-                           serial::parity_t parity           = serial::parity_none,
-                           serial::stopbits_t stopbits       = serial::stopbits_one,
-                           serial::flowcontrol_t flowcontrol = serial::flowcontrol_none);
+        CaveTalkSerialPort(const std::string &port,
+                           uint32_t baudrate,
+                           serial::Timeout timeout,
+                           serial::bytesize_t bytesize,
+                           serial::parity_t parity,
+                           serial::stopbits_t stopbits,
+                           serial::flowcontrol_t flowcontrol);
         ~CaveTalkSerialPort(void);
         CaveTalk_Error_t Send(const void *const data, const size_t size);
         CaveTalk_Error_t Receive(void *const data, const size_t size, size_t *const bytes_received);
