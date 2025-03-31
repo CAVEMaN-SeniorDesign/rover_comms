72f99beef7076e1f0b969e23596bbca27ea7d047
delta-12 <btesq235@gmail.com>
2025-03-31 16:51:02 -0400

    SD-334 uncrustify listener code

--- src/rover_comms_listener.cpp
+++ src/rover_comms_listener.cpp
@@ -4,25 +4,29 @@
 
 
 // ListenerCallbacks implementation
-RoverCommsListener::RoverCommsListener(std::shared_ptr<RoverComm> node):rover_comm_node_(node) // assigns to rover_comm_node_
+RoverCommsListener::RoverCommsListener(std::shared_ptr<RoverComm> node) : rover_comm_node_(node) // assigns to rover_comm_node_
 {
 
 }
 
 void RoverCommsListener::HearOogaBooga(const cave_talk::Say ooga_booga)
 {
-    std::string output  = "Heard ";
+    std::string output = "Heard ";
 
-    if (ooga_booga==cave_talk::SAY_OOGA){
+    if (ooga_booga == cave_talk::SAY_OOGA)
+    {
         CaveTalk_Error_t error_Ooga = (rover_comm_node_->talker)->SpeakOogaBooga(cave_talk::SAY_BOOGA);
-        if (error_Ooga == CAVE_TALK_ERROR_NONE){
+        if (error_Ooga == CAVE_TALK_ERROR_NONE)
+        {
             output = output + "OOGA, said BOOGA";
         }
-        else{
+        else
+        {
             output = "Ooga error";
         }
     }
-    else if (ooga_booga==cave_talk::SAY_BOOGA){
+    else if (ooga_booga == cave_talk::SAY_BOOGA)
+    {
         rover_comm_node_->waiting_booga = false;
         // if(rover_comm_node_->speak_timer_){
         //     rover_comm_node_->speak_timer_->cancel(); // stops timer callbacks
@@ -49,7 +53,9 @@ void RoverCommsListener::HearLights(const bool headlights)
     RCLCPP_INFO(rover_comm_node_->get_logger(), "unk!");
 }
 
-void RoverCommsListener::HearConfigEncoder(const cave_talk::ConfigEncoder &encoder_wheel_0, const cave_talk::ConfigEncoder &encoder_wheel_1, const cave_talk::ConfigEncoder &encoder_wheel_2, const cave_talk::ConfigEncoder &encoder_wheel_3){}
+void RoverCommsListener::HearConfigEncoder(const cave_talk::ConfigEncoder &encoder_wheel_0, const cave_talk::ConfigEncoder &encoder_wheel_1, const cave_talk::ConfigEncoder &encoder_wheel_2, const cave_talk::ConfigEncoder &encoder_wheel_3)
+{
+}
 
 void RoverCommsListener::HearArm(const bool arm)
 {
@@ -80,24 +86,24 @@ void RoverCommsListener::HearOdometry(const cave_talk::Imu &IMU, const cave_talk
 {
     auto msg = rover_interfaces::msg::Odomplot();
 
-    msg.total_pulses_encoder_wheel_0 = encoder_wheel_0.total_pulses();
+    msg.total_pulses_encoder_wheel_0      = encoder_wheel_0.total_pulses();
     msg.rate_rads_per_sec_encoder_wheel_0 = encoder_wheel_0.rate_radians_per_second();
 
-    msg.total_pulses_encoder_wheel_1 = encoder_wheel_1.total_pulses();
+    msg.total_pulses_encoder_wheel_1      = encoder_wheel_1.total_pulses();
     msg.rate_rads_per_sec_encoder_wheel_1 = encoder_wheel_1.rate_radians_per_second();
 
-    msg.total_pulses_encoder_wheel_2 = encoder_wheel_2.total_pulses();
+    msg.total_pulses_encoder_wheel_2      = encoder_wheel_2.total_pulses();
     msg.rate_rads_per_sec_encoder_wheel_2 = encoder_wheel_2.rate_radians_per_second();
 
-    msg.total_pulses_encoder_wheel_3 = encoder_wheel_3.total_pulses();
+    msg.total_pulses_encoder_wheel_3      = encoder_wheel_3.total_pulses();
     msg.rate_rads_per_sec_encoder_wheel_3 = encoder_wheel_3.rate_radians_per_second();
 
-    msg.x_accel_mpss = IMU.accel().x_meters_per_second_squared(); 
-    msg.y_accel_mpss = IMU.accel().y_meters_per_second_squared();
-    msg.z_accel_mpss = IMU.accel().z_meters_per_second_squared();
-    msg.roll_rads_per_sec = IMU.gyro().roll_radians_per_second();
+    msg.x_accel_mpss       = IMU.accel().x_meters_per_second_squared();
+    msg.y_accel_mpss       = IMU.accel().y_meters_per_second_squared();
+    msg.z_accel_mpss       = IMU.accel().z_meters_per_second_squared();
+    msg.roll_rads_per_sec  = IMU.gyro().roll_radians_per_second();
     msg.pitch_rads_per_sec = IMU.gyro().pitch_radians_per_second();
-    msg.yaw_rads_per_sec = IMU.gyro().yaw_radians_per_second();
+    msg.yaw_rads_per_sec   = IMU.gyro().yaw_radians_per_second();
 
     rover_comm_node_->odom_read_pub_->publish(msg);
 
